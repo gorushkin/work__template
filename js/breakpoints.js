39de85e9a015257849759bfb62b6ab95d3286d28
gorushkin <gorushkin@gmail.com>
2019-10-08 15:54:27 +0300

    немного переделал

--- js/breakpoints.js
+++ js/breakpoints.js
@@ -1,4 +1,5 @@
-const breakpointsList = [
+(function() {
+  const breakpointsList = [
   {name: 'mobile', min: 0, max: 479},
   {name: 'wide-mobile', min: 480, max: 767},
   {name: 'tablet', min: 768, max: 991},
@@ -7,8 +8,13 @@ const breakpointsList = [
   {name: 'desktop-full', min: 1600, max: 25000},
 ]
 
+const html = '<div class="row row--top"></div><div class="row row--mid"><span class="js_min value value--min center ">320px</span><span class="js_name name center">Desktop</span><span class="js_max value value--max center ">479px</span></div><div class="row row--bot"><div class="conrol center">0</div><div class="conrol center">50</div><div class="conrol center">100</div><div class="conrol center"></div></div>';
+const htmlClassList = [
+  'js_min', 'js_max', 'js_name',
+]
+
 const showData = (name) => {
-  const element = document.querySelector('.js-span');
+  const element = document.querySelector('.js_name');
   element.textContent = name;
 }
 
@@ -21,21 +27,20 @@ const windowSizeHandler = () => {
 const findName = (array) => {
   const size = document.documentElement.clientWidth
   const name = array.filter(n => n.min <= size && n.max > size)[0].name;
+  console.log(name);
   return name;
 };
 
 const createSpan = (name) => {
   const fragment = document.createDocumentFragment();
   const block = document.createElement('div');
-  const span = document.createElement('span');
   block.classList.add('js_window');
-  span.classList.add('js_span');
-  span.textContent = name;
-  block.innerHTML = '<p></p>';
-  block.appendChild(span);
+  block.innerHTML = html;
   fragment.appendChild(block);
   document.body.appendChild(fragment);
-  console.log(span);
+  const span = document.querySelector('.js_name');
+  span.textContent = name;
+  // console.log(block);
 }
 
 const screenName = findName(breakpointsList);
@@ -44,3 +49,5 @@ createSpan(screenName);
 showData(screenName);
 
 window.addEventListener('resize', windowSizeHandler);
+
+})();

b9ca5842d31afeaedff431a57da007d229f946f9
gorushkin <gorushkin@gmail.com>
2019-10-08 19:31:51 +0300

    ввел списо кэлементов

--- js/breakpoints.js
+++ js/breakpoints.js
@@ -10,22 +10,21 @@
 
 const html = '<div class="row row--top"></div><div class="row row--mid"><span class="js_min value value--min center ">320px</span><span class="js_name name center">Desktop</span><span class="js_max value value--max center ">479px</span></div><div class="row row--bot"><div class="conrol center">0</div><div class="conrol center">50</div><div class="conrol center">100</div><div class="conrol center"></div></div>';
 const htmlClassList = [
-  'js_min', 'js_max', 'js_name',
+  'js_min', 'js_max', 'js_name', 'js_menu',
 ]
 
+// let varList;
 
 const createVarList = (list) => {
-  return list.map(n => {
-    let q = {};
-    q.class = n;
-    q.var = document.querySelector('.' + n);
-  return q;
+  let result = {};
+  list.forEach(element => {
+    result[element] = document.querySelector('.' + element);
   });
+  return result;
 };
 
 const showData = (name) => {
-  const element = document.querySelector('.js_name');
-  element.textContent = name;
+  varList.js_name.textContent = name;
 }
 
 const windowSizeHandler = () => {
@@ -41,24 +40,21 @@ const findName = (array) => {
   return name;
 };
 
-const createSpan = (name) => {
+const createSpan = () => {
   const fragment = document.createDocumentFragment();
   const block = document.createElement('div');
   block.classList.add('js_window');
   block.innerHTML = html;
   fragment.appendChild(block);
   document.body.appendChild(fragment);
-  const span = document.querySelector('.js_name');
-  span.textContent = name;
-  const myList = createVarList(htmlClassList);
-  // console.log(myList);
-  // console.log(block);
+  varList = createVarList(htmlClassList);
+  const screenName = findName(breakpointsList);
+  varList.js_name.textContent = name;
+  showData(screenName);
 }
 
-const screenName = findName(breakpointsList);
 
-createSpan(screenName);
-showData(screenName);
+createSpan();
 
 window.addEventListener('resize', windowSizeHandler);
 
